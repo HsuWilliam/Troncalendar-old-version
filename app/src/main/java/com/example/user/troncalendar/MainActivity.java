64c777c35fae991cbd6e487dc813ab86712551f6
Eric Lin <ericlin870621@gmail.com>
2018-05-13 10:44:08 +0800

    course.jave profile.xml

--- app/src/main/java/com/example/user/troncalendar/MainActivity.java
+++ app/src/main/java/com/example/user/troncalendar/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.user.troncalendar;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,20 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         // test merges
+        Button button = (Button) findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startNextPage();
+            }
+        });
     }
+
+    private void startNextPage(){
+        Intent intent = new Intent();
+        intent.setClass(this , course.class);
+        startActivity(intent);
+    }
+
 }
---
+++ app/src/main/java/com/example/user/troncalendar/course.java
@@ -0,0 +1,12 @@
+package com.example.user.troncalendar;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class course extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.course);
+    }
+}
