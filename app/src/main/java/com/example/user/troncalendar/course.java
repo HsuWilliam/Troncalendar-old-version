42a64d0d4b3d5c8bf59c83b65b9b1aa11fcbf600
Eric Lin <ericlin870621@gmail.com>
2018-05-14 20:17:30 +0800

    layout & class

--- app/src/main/java/com/example/user/troncalendar/course.java
+++ app/src/main/java/com/example/user/troncalendar/course.java
@@ -1,12 +1,29 @@
 package com.example.user.troncalendar;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
 
 public class course extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.course);
+        ImageView imageView = findViewById(R.id.imageView9);
+
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startNextPage();
+            }
+        });
+    }
+
+    private void startNextPage(){
+        Intent intent = new Intent();
+        intent.setClass(this , profile.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/user/troncalendar/profile.java
+++ app/src/main/java/com/example/user/troncalendar/profile.java
@@ -1,12 +1,30 @@
 package com.example.user.troncalendar;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
 
 public class profile extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.profile);
+
+        ImageView imageView = findViewById(R.id.imageView6);
+
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startNextPage();
+            }
+        });
+    }
+
+    private void startNextPage(){
+        Intent intent = new Intent();
+        intent.setClass(this , course.class);
+        startActivity(intent);
     }
 }
